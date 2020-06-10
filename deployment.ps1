76e6ba6476103003cb1d2c9a81f835f6ba278f76
himanjanpati <himanjan.pati@outlook.com>
2020-06-10 19:42:51 +0100

    Delete deployment.ps1
    deleting

--- deployment.ps1
+++
@@ -1,4 +0,0 @@
-$rg = "AZarmresources"
-New-AzResourceGroup -Name $rg -Location "UKSouth"
-
-New-AzResourceGroupDeployment -ResourceGroupName $rg -TemplateFile ./azuredeploy.json -TemplateParameterFile ./azuredeploy.parameters.json
