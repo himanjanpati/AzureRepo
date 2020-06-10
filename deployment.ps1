e903ba1489c63fb2b483c6976e35b52f0e7605b3
Himanjan Pati <Himanjan.pati@outlook.com>
2020-06-10 12:53:06 +0100

    init

--- deployment.ps1
+++ deployment.ps1
@@ -1,4 +1,4 @@
 $rg = "AZarmresources"
-New-AzResourceGroup -Name $rg -Location "UKSouth"
+#New-AzResourceGroup -Name $rg -Location "UKSouth" - No need to repat this for incremental deployment
 
 New-AzResourceGroupDeployment -ResourceGroupName $rg -TemplateFile ./azuredeploy.json -TemplateParameterFile ./azuredeploy.parameters.json
