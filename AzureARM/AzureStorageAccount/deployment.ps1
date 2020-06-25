26d6648b792832530c5a39e0adbf1e93dbc91044
Himanjan Pati <Himanjan.pati@outlook.com>
2020-06-26 00:34:54 +0100

    Parameterized ARM template

--- AzureARM/AzureStorageAccount/deployment.ps1
+++ AzureARM/AzureStorageAccount/deployment.ps1
@@ -1,4 +1,4 @@
 $rg = 'ARMresources'
 new-AzResourceGroup -name $rg -Location UKSouth
 
-New-AzResourceGroupDeployment -Name 'arm-storage' -ResourceGroupName $rg -TemplateFile 'Azuredeploy.json'
+New-AzResourceGroupDeployment -Name 'arm-storage' -ResourceGroupName $rg -TemplateFile 'Azuredeploy.json' -storageName 'azarmstorageaccount'
