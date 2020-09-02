d27619ab593b61e5d8ec4182b7de3a90584aa61a
Himanjan Pati <Himanjan.pati@outlook.com>
2020-09-02 23:24:03 +0100

    Changed the function name and changed to POST HTTP request

---
+++ GitHubMonitorApp/AzFunctionHttpTrigger/GitHubHttpTrigger.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AzFunctionHttpTrigger
+{
+    public static class GitHubHttpTrigger
+    {
+        [FunctionName("GitHubHttpTrigger")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("Github monitor processed on action");
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+            log.LogInformation(requestBody);
+
+            return new OkResult();
+        }
+    }
+}
